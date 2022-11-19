7b0cea16ca03f472ad2dcd4a16194c11ad1ce5f8
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-11-19 11:27:38 -0800

    Seperates opencilk install + creates cache for file download.

---
+++ download-opencilk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+VERSION='v2.0'
+kernelName="$(uname -s)"
+PLATFORM='x86_64'
+OPENCILK_DOWNLOAD_PREFIX="https://github.com/OpenCilk/opencilk-project/releases/download/opencilk"
+
+case "$kernelName" in
+    Darwin*)
+        OS="Apple-Darwin"
+        ;;
+    Linux*)
+        OS="Linux-Ubuntu-22.04"
+        ;;
+esac
+
+URL=${OPENCILK_DOWNLOAD_PREFIX}/${VERSION}/OpenCilk-2.0.0-${PLATFORM}-${OS}.sh
+
+curl -fsSL ${URL} -o opencilk_install.sh
+
+if [[ $? != 0 ]]; then
+    echo "Failed to download OpenCilk Shell Archive: ${URL}"
+    exit 1
+fi
--- setup-opencilk.sh
+++ setup-opencilk.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
-VERSION='v2.0'
-kernelName="$(uname -s)"
-PLATFORM='x86_64'
-OPENCILK_DOWNLOAD_PREFIX="https://github.com/OpenCilk/opencilk-project/releases/download/opencilk"
-
-case "$kernelName" in
-    Darwin*)
-        OS="Apple-Darwin"
-        ;;
-    Linux*)
-        OS="Linux-Ubuntu-22.04"
-        ;;
-esac
-
-URL=${OPENCILK_DOWNLOAD_PREFIX}/${VERSION}/OpenCilk-2.0.0-${PLATFORM}-${OS}.sh
-
-curl -fsSL ${URL} -o opencilk_install.sh
-
-if [[ $? != 0 ]]; then
-    echo "Failed to download OpenCilk Shell Archive: ${URL}"
-    exit 1
-fi
-
 mkdir /opt/opencilk
 chmod +x opencilk_install.sh
 sudo ./opencilk_install.sh --skip-license --prefix=/opt/opencilk --exclude-subdir
 rm ./opencilk_install.sh
+
