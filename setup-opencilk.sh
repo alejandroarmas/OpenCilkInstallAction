c976bccae04ce8e1836df14275e858849db5940f
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-11-19 14:05:57 -0800

    Attempting to resolve 'libstdc++6' still.

--- setup-opencilk.sh
+++ setup-opencilk.sh
@@ -5,6 +5,8 @@ sudo ./opencilk_install.sh --skip-license --prefix=/opt/opencilk --exclude-subdi
 rm ./opencilk_install.sh
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test # Ignore if not ubuntu
 sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 sudo apt-get install gcc-4.9
-sudo apt-get install --upgrade libstdc++6
+sudo apt-get install --only-upgrade libstdc++6
 
