020a299784b4958e4e1a5fb1a749d2c0bb35f673
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-11-19 13:50:59 -0800

    Upgrading libstdc++6 command altered.

--- setup-opencilk.sh
+++ setup-opencilk.sh
@@ -6,5 +6,5 @@ rm ./opencilk_install.sh
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test # Ignore if not ubuntu
 sudo apt-get update
 sudo apt-get install gcc-4.9
-sudo apt-get install --only-upgrade libstdc++6
+sudo apt-get install --upgrade libstdc++6
 
