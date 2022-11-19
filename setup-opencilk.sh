1a1fb5bff6fbf0747aebf47418acd1b4a3a35fb0
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-11-19 10:56:20 -0800

    Now accepts license upon install prompt.

--- setup-opencilk.sh
+++ setup-opencilk.sh
@@ -22,6 +22,7 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
+mkdir /opt/opencilk
 chmod +x opencilk_install.sh
-sudo ./opencilk_install.sh
+sudo ./opencilk_install.sh --skip-license --prefix=/opt/opencilk --exclude-subdir
 rm ./opencilk_install.sh
