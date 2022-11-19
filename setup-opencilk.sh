6f200d98f3b620f967a62e2e9a4f67ae3017487c
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2022-11-19 11:46:37 -0800

    attempt to find 'GLIBCXX_3.4.30'.

--- setup-opencilk.sh
+++ setup-opencilk.sh
@@ -3,4 +3,8 @@ mkdir /opt/opencilk
 chmod +x opencilk_install.sh
 sudo ./opencilk_install.sh --skip-license --prefix=/opt/opencilk --exclude-subdir
 rm ./opencilk_install.sh
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test # Ignore if not ubuntu
+sudo apt-get update
+sudo apt-get install gcc-4.9
+sudo apt-get upgrade libstdc++6
 
